c680de78a36d4691753073de4a79ec92eec1aa63
Jeffrey Cheng <jeffrey.k.cheng@dartmouth.edu>
2024-10-08 09:03:01 -0400

    moved defaultSocketPort

--- source/microservice.go
+++ source/microservice.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"errors"
+
 	"github.com/Dartmouth-OpenAV/microservice-framework/framework"
 )
 
 // Define device specific globals
-var defaultSocketPort = 4352   // Global Cache's default socket port
 // var defaultLineDelimiter = 0xA // 0xA==\n==linefeed, 0xD==\r==carriagereturn
 
 func setFrameworkGlobals() {
 	// globals that change modes in the microservice framework:
+	framework.DefaultSocketPort = 4352 // Global Cache's default socket port
 	framework.CheckFunctionAppendBehavior = "Remove older instance"
 	framework.RegisterMainGetFunc(doDeviceSpecificGet)
 	framework.RegisterMainSetFunc(doDeviceSpecificSet)
@@ -21,27 +22,28 @@ func setFrameworkGlobals() {
 // socketKey is the network connection for the framework to use to communicate with the device.
 // setting is the first parameter in the URI.
 // arg1 are the second and third parameters in the URI.
-//   Example PUT URIs that will result in this function being invoked:
-// 	 ":address/:setting/"
-//   ":address/:setting/:arg1"
-//   ":address/:setting/:arg1/:arg2"
+//
+//	  Example PUT URIs that will result in this function being invoked:
+//		 ":address/:setting/"
+//	  ":address/:setting/:arg1"
+//	  ":address/:setting/:arg1/:arg2"
 func doDeviceSpecificSet(socketKey string, setting string, arg1 string, arg2 string, arg3 string) (string, error) {
 	function := "doDeviceSpecificSet"
 
 	// Add a case statement for each set function your microservice implements.  These calls can use 0, 1, or 2 arguments.
 	switch setting {
-		case "power":
-			return setPower(socketKey, arg1)
-		case "volume":
-			return setVolume(socketKey, arg1)
-		case "videoroute":
-			return setVideoRoute(socketKey, arg1, arg2)
-		case "videomute":
-			return setVideoMute(socketKey, arg1, arg2)
-		case "audiomute":
-			return setAudioMute(socketKey, arg1, arg2)		
-		case "audioandvideomute":
-			return setAudioAndVideoMute(socketKey, arg1, arg2)
+	case "power":
+		return setPower(socketKey, arg1)
+	case "volume":
+		return setVolume(socketKey, arg1)
+	case "videoroute":
+		return setVideoRoute(socketKey, arg1, arg2)
+	case "videomute":
+		return setVideoMute(socketKey, arg1, arg2)
+	case "audiomute":
+		return setAudioMute(socketKey, arg1, arg2)
+	case "audioandvideomute":
+		return setAudioAndVideoMute(socketKey, arg1, arg2)
 	}
 
 	// If we get here, we didn't recognize the setting.  Send an error back to the config writer who had a bad URL.
@@ -55,27 +57,29 @@ func doDeviceSpecificSet(socketKey string, setting string, arg1 string, arg2 str
 // socketKey is the network connection for the framework to use to communicate with the device.
 // setting is the first parameter in the URI.
 // arg1 are the second and third parameters in the URI.
-//   Example GET URIs that will result in this function being invoked:
-// 	 ":address/:setting/"
-//   ":address/:setting/:arg1"
-//   ":address/:setting/:arg1/:arg2"
+//
+//	  Example GET URIs that will result in this function being invoked:
+//		 ":address/:setting/"
+//	  ":address/:setting/:arg1"
+//	  ":address/:setting/:arg1/:arg2"
+//
 // Every microservice using this golang microservice framework needs to provide this function to invoke functions to do gets.
 func doDeviceSpecificGet(socketKey string, setting string, arg1 string, arg2 string) (string, error) {
 	function := "doDeviceSpecificGet"
 
 	switch setting {
-		case "power":
-			return getPower(socketKey) // arg1 is name in this case
-		case "videoroute":
-			return getVideoRoute(socketKey, arg1) // arg1 is name in this case
-		case "inputs":
-			return getInputs(socketKey, arg1) // arg1 is name in this case
-		case "videomute":
-			return getVideoMute(socketKey, arg1) // arg1 is name in this case
-		case "audiomute":
-			return getAudioMute(socketKey, arg1) // arg1 is name in this case
-		case "audioandvideomute":
-			return getAudioAndVideoMute(socketKey, arg1) // arg1 is name in this case
+	case "power":
+		return getPower(socketKey) // arg1 is name in this case
+	case "videoroute":
+		return getVideoRoute(socketKey, arg1) // arg1 is name in this case
+	case "inputs":
+		return getInputs(socketKey, arg1) // arg1 is name in this case
+	case "videomute":
+		return getVideoMute(socketKey, arg1) // arg1 is name in this case
+	case "audiomute":
+		return getAudioMute(socketKey, arg1) // arg1 is name in this case
+	case "audioandvideomute":
+		return getAudioAndVideoMute(socketKey, arg1) // arg1 is name in this case
 	}
 
 	// If we get here, we didn't recognize the setting.  Send an error back to the config writer who had a bad URL.
@@ -86,7 +90,6 @@ func doDeviceSpecificGet(socketKey string, setting string, arg1 string, arg2 str
 }
 
 func main() {
-   setFrameworkGlobals()
-   framework.Startup()
+	setFrameworkGlobals()
+	framework.Startup()
 }
-
